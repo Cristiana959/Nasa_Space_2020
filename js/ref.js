c5142f495c8fabf4b9931b02bed542b4ac3534cd
Sebastian Nae <naesebi2000@gmail.com>
2020-09-25 23:50:39 +0300

    added ref, team, with reading from files

---
+++ js/ref.js
@@ -0,0 +1,17 @@
+function Render_ref() {
+    fetch('../ref.txt')
+        .then(response => response.text())
+        .then(data => {
+            var text = data.split('\n');
+            let ref = document.getElementById('ref');
+            for (let i = 0; i < text.length; i++) {
+                let p = document.createElement("p");
+                let br = document.createElement("br");
+                let node = document.createTextNode((i + 1) + " " + text[i]);
+                p.appendChild(node);
+                ref.appendChild(p);
+                ref.appendChild(br);
+            }
+        });
+}
+Render_ref();
--- js/team.js
+++ js/team.js
@@ -1,3 +1,36 @@
-function Team_render() {
-
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
 }
+
+function generateTable(table, data) {
+    for (let element of data) {
+        let row = table.insertRow();
+        for (key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+    }
+}
+
+function Team_render() {
+    let profile_infos = [
+        { name: "Monte Falco", picture: 1658, link: "Parco Foreste Casentinesi" },
+        { name: "Monte Falterona", picture: 1654, link: "Parco Foreste Casentinesi" },
+        { name: "Monte Falterona", picture: 1654, link: "Parco Foreste Casentinesi" }
+    ];
+    let table = document.querySelector("table");
+    let data = Object.keys(profile_infos[0]);
+    generateTableHead(table, data);
+    generateTable(table, profile_infos);
+}
+
+
+Team_render();
